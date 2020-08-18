c976f5d177f26aab31f62d832c7215f41834bee1
hnguyen <hng@digital-commerce-experts.com>
2020-08-18 11:37:50 +0200

    console log

--- src/input.vue
+++ src/input.vue
@@ -7,6 +7,33 @@
 
   export default {
     mixins: [mixin],
+    mounted() {
+      // Fetch block options
+      const { values } = this._props
+      console.log(values);
+      
+      const fieldsNode = document.querySelectorAll('[data-field]');
+      const typeField = document.querySelector('[data-field=type]');
+      const options = this.getOptions();
+
+      // hide all fields until the user choose a type
+      this.hideAll(fieldsNode)
+
+      // Show the default value if present
+      this.showFields(options[values.type])
+
+      if (typeField) {
+        typeField.addEventListener('change', (e) => {
+          const value = e.target.value
+
+          // Hide all the fields on change
+          this.hideAll(fieldsNode)
+
+          // show fields from the options
+          this.showFields(options[value])
+        })
+      }
+    },
     methods: {
       emitValue(event) {
         const value = event.target.value;

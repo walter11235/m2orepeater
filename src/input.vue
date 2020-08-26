f2599dfd2503263b85909bba14cb434c44e0fab4
hnguyen <hng@digital-commerce-experts.com>
2020-08-26 23:07:41 +0200

    df

--- src/input.vue
+++ src/input.vue
@@ -23,7 +23,7 @@
     data() {
       return {
         collectionName: "",
-        options: null,
+        options: this.callAP,
         selectedvalue: ""
       };
     },
@@ -32,26 +32,7 @@
     },
     mounted() {
       
-        this.findCollectionValue();
-        const fullURL = this.findURL();
-        var dropdownAlternative = [];
-        fetch(fullURL) // Call the fetch function passing the url of the API as a parameter
-        .then((resp) => resp.json())
-        .then(function(response) {
-            // Your code for handling the data you get from the API
-            console.log("inside fetch");
-            console.log(response);
-            response["data"].forEach(element => {
-              dropdownAlternative.push(element.identifier);
-            });
-            console.log("after push");
-            console.log(dropdownAlternative);
-            this.options = dropdownAlternative;
-        })
-        .catch(function(error) {
-            // This is where you run code if the server returns any errors
-            console.error("Error:", error);
-        });
+      // this.options = this.callAPI;
       
       
       /*

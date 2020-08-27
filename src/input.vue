74d52f68a561a499c7458de3eac63e9f28637851
hnguyen <hng@digital-commerce-experts.com>
2020-08-27 17:34:09 +0200

    sdf

--- src/input.vue
+++ src/input.vue
@@ -137,7 +137,7 @@
 
       emitValue(event) {
         //const value = event.target.value;
-        var value = this.options[event];
+        var value = options[event];
         console.log("options array: ");
         console.log(this.options);
 
@@ -147,7 +147,7 @@
         console.log(event.target);
         console.log("value");
         console.log(event.target.value);
-        console.log("value to be emitted; ");
+        console.log("value to be emitted: ");
         console.log(value);
         this.$emit("input", this.options[event]);
       },

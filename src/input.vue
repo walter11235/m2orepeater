be7cd8122c8a7c0970f5940f8161a8919f1d426e
hnguyen <hng@digital-commerce-experts.com>
2020-08-27 15:09:26 +0200

    sdf

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-     <v-select v-model="selectedvalue" :items="options"  @input="$emit('input', $event.target.value)" >
+     <v-select v-model="selectedvalue" :options="options"  @input="$emit('input', $event.target.value)" >
      </v-select>
      <div>{{selectedvalue}}</div>
     <button v-on:click="foo">foo</button>
